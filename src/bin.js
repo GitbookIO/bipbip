68ef6ab60ad8611a05092379a0a98d3634d469c3
Milan Hauth <milahu@gmail.com>
2022-09-14 14:52:52 +0200

    fix parsing argv

--- src/bin.js
+++ src/bin.js
@@ -1,7 +1,7 @@
 import cluster from 'cluster';
 import path from 'path';
 import { globby } from 'globby';
-import { program } from 'commander';
+import { program as commanderProgram } from 'commander';
 import { ConsoleReporter, BackgroundReporter } from './reporters/index.js';
 import { runBenchmark } from './benchmark.js';
 import { getBenchmark, suite, scenario } from './globals.js';
@@ -10,12 +10,17 @@ import { saveResult, loadResult } from './file-report.js'; //import packageJSON 
 const DEFAULT_FILES = ['**/__benchmarks__/*.js'];
 const IGNORED_FILES = ['**/node_modules'];
 
+const debug = false;
+
 if (cluster.isMaster) {
     // Define global variables used by scenarios
     global.suite = suite;
     global.scenario = scenario; // Define command line spec
 
-    program //.version(packageJSON.version, '-v, --version')
+    debug && console.log(`bipbip: parse argv: ${JSON.stringify(process.argv)}`);
+
+    commanderProgram
+        //.version(packageJSON.version, '-v, --version')
         .usage('[options] <file...>')
         .option(
             '-d, --duration [ms]',
@@ -28,6 +33,7 @@ if (cluster.isMaster) {
         .option('-s, --save [file]', 'save result to a JSON file')
         .option('-c, --compare [file]', 'compare result with previous results')
         .parse(process.argv);
+
     main().then(
         () => {
             //process.exit(0);
@@ -75,10 +81,21 @@ if (cluster.isMaster) {
  */
 
 async function main() {
-    const inputFiles = program.args.length == 0 ? DEFAULT_FILES : program.args;
+    const program = commanderProgram.opts();
+    program.args = commanderProgram.args;
+    Object.freeze(program);
+    debug && console.dir(program);
+
+    const inputFiles = (program.args && program.args.length > 0) ? program.args : DEFAULT_FILES;
+    debug && console.dir({ DEFAULT_FILES });
+    debug && console.dir({ inputFiles });
     const paths = await globby(inputFiles, {
         ignore: IGNORED_FILES
     });
+    debug && console.dir({ paths });
+    if (paths.length == 0) {
+        throw new Error(`no benchmark files with pattern ${JSON.stringify(inputFiles)}`);
+    }
 
     for (const filePath of paths) {
         await import(path.resolve(process.cwd(), filePath));
@@ -97,7 +114,13 @@ async function main() {
     };
     const result = await runBenchmark(input, previous, options);
 
+    debug && console.log('\n' + `bipbip: result done`)
+
     if (program.save) {
+        debug && console.log('\n' + `bipbip: save ${program.save} ...`)
         await saveResult(path.resolve(process.cwd(), program.save), result);
+        debug && console.log('\n' + `bipbip: save done`)
     }
+
+    debug && console.log('\n' + `bipbip: main done`)
 }
